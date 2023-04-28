804d179defc1e8db8b6ac00d2b4b3edc01d0f62d
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-29 00:19:47 +0100

    commit

--- src/main/java/dev/elma/customer_microservice_spring/CustomerMicroServiceSpringApplication.java
+++ src/main/java/dev/elma/customer_microservice_spring/CustomerMicroServiceSpringApplication.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.stream.Stream;
 
@@ -14,12 +15,14 @@ import java.util.stream.Stream;
 public class CustomerMicroServiceSpringApplication implements CommandLineRunner {
 
     CustomerRepository customerRepository;
+    private RepositoryRestConfiguration repositoryRestConfiguration;
     public static void main(String[] args) {
         SpringApplication.run(CustomerMicroServiceSpringApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+        repositoryRestConfiguration.exposeIdsFor(Customer.class);
         Stream.of("Mohammed","Ahmed","Ali","Nawal")
                 .forEach(name->{
                     Customer c=Customer.builder()
